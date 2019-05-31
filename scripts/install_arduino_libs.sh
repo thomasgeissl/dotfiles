eb9724f0544ce4df347579a1feeb98a686828fc1
Thomas Geissl <thomas.geissl@gmail.com>
2019-05-31 08:24:43 +0200

    added some frequently used arduino libraries

---
+++ scripts/install_arduino_libs.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/adafruit/Adafruit_NeoPixel.git
+git clone https://github.com/FastLED/FastLED.git
+git clone https://github.com/PaulStoffregen/Audio.git
+git clone https://github.com/PaulStoffregen/MIDI.git
+git clone https://github.com/CNMAT/OSC.git
+git clone https://github.com/bakercp/PacketSerial.git
+
