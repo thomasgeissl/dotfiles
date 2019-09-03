a54ca139e10f2ad93c6fabd5f894462453a0b180
Thomas Geissl <thomas.geissl@gmail.com>
2019-09-03 16:02:56 +0200

    added windows install script

---
+++ scripts/install.bat
@@ -0,0 +1,30 @@
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install vscode
+choco install visualstudio2017community 
+
+choco install git.install 
+choco install nodejs.install 
+
+choco install firefox 
+choco install googlechrome
+
+choco install vim 
+choco install curl
+choco install wget
+
+choco install putty 
+
+choco install arduino
+choco install fritzing 
+
+choco install slack 
+choco install skype 
+
+choco install puredata 
+choco install supercollider 
+choco install touchdesigner 
+
+choco install vlc
+choco install spotify
