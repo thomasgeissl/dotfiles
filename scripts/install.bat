9ba2683fe399f8ed69d764eef9f9b01df0b7924c
Thomas Geissl <thomas.geissl@gmail.com>
2020-04-18 20:11:22 +0200

    Update install.bat

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,33 +1,33 @@
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install vscode
-choco install visualstudio2017community 
+choco install vscode -y
+choco install visualstudio2017community -y
 
-choco install msys2
+choco install msys2 -y
 
-choco install git.install 
-choco install nodejs.install 
+choco install git.install -y
+choco install nodejs.install -y
+choco install nvm -y
 
-choco install firefox 
-choco install googlechrome
+choco install firefox -y
+choco install googlechrome -y
 
-choco install vim 
-choco install curl
-choco install wget
+choco install vim -y
+choco install curl -y
+choco install wget -y
 
-choco install putty 
+choco install putty -y
 
-choco install arduino
-choco install fritzing 
+choco install arduino -y
 
-choco install slack 
-choco install skype 
+choco install slack -y
+choco install skype -y
 
-choco install puredata 
-choco install supercollider 
-choco install touchdesigner
-choco install k-litecodecpackfull
+choco install puredata -y
+choco install supercollider -y 
+choco install touchdesigner -y
+choco install k-litecodecpackfull -y
 
-choco install vlc
-choco install spotify
+choco install vlc -y
+choco install spotify -y
