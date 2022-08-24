7ccdcc1140342d16ea126060f082be0020a98376
Thomas Geissl <thomas.geissl@gmail.com>
2022-08-24 12:29:44 +0200

    added cpp extensions

--- scripts/vscode_extensions.sh
+++ scripts/vscode_extensions.sh
@@ -1,16 +1,21 @@
 code --install-extension vscode-icons
+code --install-extension 2gua.rainbow-brackets
+code --install-extension oderwat.indent-rainbow
+code --install-extension stuart.unique-window-colors
+
 code --install-extension vscodevim.vim
-code --install-extension formulahendry.auto-close-tag
-code --install-extension formulahendry.auto-complete-tag
-code --install-extension formulahendry.auto-rename-tag
 code --install-extension rangav.vscode-thunder-client
 code --install-extension esbenp.prettier-vscode
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension TabNine.tabnine-vscode
-code --install-extension 2gua.rainbow-brackets
-code --install-extension oderwat.indent-rainbow
-code --install-extension stuart.unique-window-colors
 code --install-extension eamodio.gitlens
 code --install-extension ChakrounAnas.turbo-console-log
 code --install-extension yzhang.markdown-all-in-one
 code --install-extension sukumo28.wav-preview
+# html
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-complete-tag
+code --install-extension formulahendry.auto-rename-tag
+# cpp
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
